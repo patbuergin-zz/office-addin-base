b44fbbe88abdfc5d2ef20544ddf036113cf2568b
Patrick Buergin <hi@patrickbuergin.com>
2016-01-27 22:36:17 +0000

    improved _scss section; added client-specific coloring

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,15 +1,27 @@
 (function() {
+    /*
+     * Extract host infos from the location.
+     * Example: "?_host_Info=Powerpoint|Win32|16.01|en-US"
+     */
+    const [client, os, version, locale] =
+        window.location.search.split('=')[1].split('|');
+
+
     Office.initialize = () => {
+        $('body').addClass(client.toLowerCase());
+
         $('form')
             .removeClass('hidden')
             .on('submit', onSubmit);
     };
+
 
     function onSubmit() {
         let currText = $('#text').val();
         insertText(currText);
         return false;
     }
+
 
     function insertText(text) {
         Office.context.document.setSelectedDataAsync(text, (res) => {
